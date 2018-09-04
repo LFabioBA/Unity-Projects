3e07875631a227c4e5d4fed68f76b8a80b4af9d5
LFabioBA <42975084+LFabioBA@users.noreply.github.com>
2018-09-04 13:04:08 -0400

    Add files via upload

---
+++ Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public int damage=1;       //dano da bala
+	public float speed;        //Velocidade da bala
+	public float timeDestroy; //Tempo de destruição da bala
+
+	// Use this for initialization
+	void Start () {
+		//Destroi balla
+		Destroy(gameObject, timeDestroy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Direção da bala
+		transform.Translate (Vector2.right*speed*Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//Verifica se a bala tocou com inimigo
+		if (other.CompareTag ("Enemy")) {
+
+			EnemyController enemy = other.GetComponent<EnemyController> ();
+			//Tira vida do inimigo
+			if(enemy != null){
+				enemy.DamageEnemy (damage);
+			}
+		}
+		Destroy(gameObject);
+	}
+}
